d129edec9a591d430f333859762d98b7bfe97d1c
hyeon <hyeonjxxx@gmail.com>
2021-06-22 09:50:17 +0900

    add : 페이징 처리관련 클래스 추가

--- withusProject/src/main/java/com/kh/withus/common/model/vo/PageInfo.java
+++ withusProject/src/main/java/com/kh/withus/common/model/vo/PageInfo.java
@@ -1,7 +1,25 @@
 package com.kh.withus.common.model.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter @Getter
+@ToString
 public class PageInfo {
+
+	private int listCount;
+	private int currentPage;
+	private int pageLimit;
+	private int boardLimit;
 	
+	private int startPage;
+	private int endPage;
+	private int maxPage;	
 	
 
 }
---
+++ withusProject/src/main/java/com/kh/withus/common/template/pagination.java
@@ -0,0 +1,17 @@
+package com.kh.withus.common.template;
+
+import com.kh.withus.common.model.vo.PageInfo;
+
+public class pagination {
+
+	public static PageInfo getPageInfo(int listCount, int currentPage, int pageLimit, int boardLimit) {
+		
+		int maxPage = (int)Math.ceil((double)listCount/boardLimit);
+		int startPage = (currentPage - 1) / pageLimit * pageLimit +1;
+		int endPage;
+		if((endPage = startPage +pageLimit -1) > maxPage) {
+			endPage = maxPage;
+		}
+		return new PageInfo(listCount, currentPage, pageLimit, boardLimit, startPage, endPage, maxPage);
+	}
+}
