d97a869b773608002eb6d219f68899a086ee0360
Hye Jeong <hye.93@hotmail.com>
2021-06-28 13:51:12 +0900

    상세페이지 기능추가
    상세페이지 기능추가 -정혜

--- withusProject/src/main/java/com/kh/withus/funding/model/service/FundingService.java
+++ withusProject/src/main/java/com/kh/withus/funding/model/service/FundingService.java
@@ -2,11 +2,17 @@ package com.kh.withus.funding.model.service;
 
 import java.util.ArrayList;
 
+import com.kh.withus.funding.dto.FundingDetail;
 import com.kh.withus.funding.model.vo.Project;
 
 public interface FundingService {
 
 	// 게시판 리스트 조회
 	ArrayList<Project> selectList();
+	
+	// 상세페이지 조회
+	int increaseCount(int projectNo);
+	Project selectFunding(int projectNo);
+	ArrayList<FundingDetail> selectReward(int projectNo);
 	
 }
