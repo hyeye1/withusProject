5635a890df3cdc9e65045786a88aaaa6716f68b7
YunGyeongee <yuuung26@gmail.com>
2021-06-29 14:22:44 +0900

    category 수정. admin main 추가
    <title> :  category 수정. admin main 추가
    <author> : 윤경

--- withusProject/src/main/java/com/kh/withus/category/model/vo/Category.java
+++ withusProject/src/main/java/com/kh/withus/category/model/vo/Category.java
@@ -1,5 +1,19 @@
 package com.kh.withus.category.model.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter @Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Category {
+	
+	private int catNo;
+	private String catName;
+	private String catTag;
 
 }
