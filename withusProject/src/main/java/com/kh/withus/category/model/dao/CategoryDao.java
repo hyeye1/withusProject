7396b2264b4130e01d3c39ef9aa07b44fa7b74bd
YunGyeongee <yuuung26@gmail.com>
2021-07-15 13:36:52 +0900

    fix cate
    좀 역겨워질라함

--- withusProject/src/main/java/com/kh/withus/category/model/dao/CategoryDao.java
+++ withusProject/src/main/java/com/kh/withus/category/model/dao/CategoryDao.java
@@ -3,9 +3,11 @@ package com.kh.withus.category.model.dao;
 import java.util.List;
 
 import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
 
 import com.kh.withus.category.model.vo.Category;
 
+@Repository
 public class CategoryDao {
 	
 	public List<Category> menuList(SqlSessionTemplate sqlSession, Category ct){
